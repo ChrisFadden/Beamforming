ed81704d9be4b1756efd201b46b70921ad70d6a8
ChrisFadden <chris_fadden@yahoo.com>
2017-09-27 22:19:50 -0400

    Reformatted Nec2hdf5.py

--- Analysis/Nec2hdf5.py
+++ Analysis/Nec2hdf5.py
@@ -1,73 +1,146 @@
+#*************************
+#   Nec2hdf5.py
+#
+#   This reads a NEC output file that has used the PT
+#card to print currents at the feed of antenna elements
+#in an array, one PT card per element.  The currents are
+#stored in an HDF5 file for easy use in MATLAB, Python or C
+#
+#   TO DO:
+#       Add Polarization
+#       See if it can all be done in one pass?
+#           splitting it up takes slightly more time
+#           easier to follow though...
+#*************************
+
 import h5py 
 import numpy as np
 import re
 
 f = open("Test.out","r") 
+
+#Normalize so that at each frequency max is magnitude 1
 Normalize = True
+
 #***************************
 #   Create HDF5 file
 #***************************
 f5 = h5py.File("testnec.h5","w")
-gFreq = f5.create_group("Frequency")
 
-while(True):
+#*****************
+#   Get Parameters
+#*****************
+#  Frequencies 
+#  Azimuths
+#  Elevations
+#  Number of array elements
+#
+#  Does not currently
+#  use polarization
+#
+#*****************
+freq = set()
+azm = set()
+elev = set()
+Elem = set()
 
+
+ii = 1
+for line in f:
     #*************************
     #   Find Frequency in MHz
     #*************************
-    for line in f:
-        if("FREQUENCY" in line):
-            #stop for looop 
-            break 
-    else:   
-        #EOF stop while loop
-        break   
+    if("FREQUENCY" in line): 
+        #Parse the number from NEC
+        freq_i = re.findall('[\d.]+',f.readline())
+        freq_i = round(float(freq_i[0]) * 10**(float(freq_i[1])),4)
+        freq.add(freq_i) 
+    
+    if("THETA" in line):
+        f.readline() #Skip line
+        
+        #Parse out useful data
+        for data in f:
+            if(not data.strip() or "DATA" in data):
+                break
+            dataArray = re.findall('[+-]?\d+(?:\.\d+)?',data) 
+            elev.add(float(dataArray[0]))
+            azm.add(float(dataArray[1]))
+            Elem.add(int(dataArray[5]))
 
-    #Parse the number from NEC
-    freq = re.findall('[\d.]+',f.readline())
-    freq = round(float(freq[0]) * 10**(float(freq[1])),4)
-       
-    #create Group for the frequency found
-    gPattern = gFreq.create_group(str(freq))
-    
-    #**************************
-    #   Parse Radiation Pattern
-    #**************************
-    Elev = []
-    Azm = []
-    EthMag = []
-    EthPhase = []
-    EphiMag = []
-    EphiPhase = []
-    for line in f:
-        if("RADIATION PATTERNS" in line):
-            for x in range(4):  f.readline()
-            break
-    for line in f: 
-        if(not line.strip()):
-            break
-        data = re.findall('[+-]?\d+(?:\.\d+)?',line)
-    
-        Elev.append(float(data[0])) 
-        Azm.append(float(data[1]))
-  
-        EthMag.append(float(data[7]) * 10**(float(data[8])))
-        EthPhase.append(float(data[9]))
-    
-        EphiMag.append(float(data[10]) * 10**(float(data[11])))
-        EphiPhase.append(float(data[12]))
-    
-    if(Normalize):
-        EthMag = np.asarray(EthMag) - np.max(EthMag)
-        EphiMag = np.asarray(EphiMag) - np.max(EphiMag)
-    #Place Radiation Patterns in a group in the HDF5 file 
-    gPattern.create_dataset("Elevation",data = np.asarray(Elev))
-    gPattern.create_dataset("Azimuth",data = np.asarray(Azm))
-    gPattern.create_dataset("ElevMagnitude",data = np.asarray(EthMag))
-    gPattern.create_dataset("ElevPhase",data = np.asarray(EthPhase))
-    gPattern.create_dataset("AzmMagnitude",data = np.asarray(EphiMag))
-    gPattern.create_dataset("AzmPhase",data = np.asarray(EphiPhase))
+#Reset to top of file
+f.seek(0)
 
+freq = sorted(list(freq))
+elev = sorted(list(elev))
+azm = sorted(list(azm))
+Elem = sorted(list(Elem))
+
+
+#******************************
+#   Create HDF5 File Structure
+#
+#   /Freq/Elev/Elem Mag[Azm]
+#                   Phase[Azm]
+#******************************
+f5.create_group("Frequency (MHz)")
+f5.create_dataset("listFreq", data = np.asarray(freq))
+f5.create_dataset("listElev", data = np.asarray(elev))
+f5.create_dataset("listAzm", data = np.asarray(azm))
+for ff in freq:
+    gFreq = f5.create_group(str(ff))
+    gFreq.create_group("Elevation (degrees)")
+    for th in elev:
+        gElev = gFreq.create_group(str(th))
+        gElev.create_group("Elements") 
+        for ii in Elem:
+            gElem = gElev.create_group(str(ii))
+
+#**************************
+#   Put Mag/Phase data into
+#   the HDF5 file
+#**************************
+mag = np.zeros((len(elev),len(azm)))
+phase = np.zeros((len(elev),len(azm)))
+
+freqIdx = 0
+elemIdx = 0
+for line in f:
+    
+    if(freqIdx >= len(freq)):
+        freqIdx = 0
+        elemIdx = elemIdx + 1
+
+    #Parse nearly identital to before
+    if("THETA" in line):
+        f.readline()
+
+        #Need to iterate over [Elev,Azm]
+        ii = 0
+        jj = 0
+        for data in f:      
+            #Find end of data
+            if(not data.strip() or "DATA" in data):
+                break
+            #Regex out numbers 
+            dataArray = re.findall('[+-]?\d+(?:\.\d+)?',data)
+            
+            #make mag[elev,azm] array
+            mag[jj,ii] = float(dataArray[2]) * 10**(float(dataArray[3]))
+            phase[jj,ii] = float(dataArray[4])
+            if(jj < len(elev)-1):
+                jj = jj + 1
+            else:
+                jj = 0 
+                ii = ii + 1  
+        
+        #Place array in the dataset
+        for th in range(len(elev)):
+            gElem = f5[str(freq[freqIdx])][str(elev[th])][str(Elem[elemIdx])]
+            gElem.create_dataset("Magnitude",data = mag[th,:])
+            gElem.create_dataset("Phase", data = phase[th,:])
+        freqIdx = freqIdx + 1
+        
 f.close()
 
 
