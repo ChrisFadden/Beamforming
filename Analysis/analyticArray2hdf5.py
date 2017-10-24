b26437dee3c7c71c4f658cbf0f90c61332d7f64d
George Burdell <gburdell@localhost.localdomain>
2017-10-24 14:36:35 -0400

    Implemented ULA

--- Analysis/analyticArray2hdf5.py
+++ Analysis/analyticArray2hdf5.py
@@ -17,12 +17,50 @@ cc = 299.792458
 #d is spacing
 #k is wavenumber (can be a vector for multiple frequencies)
 def ULA(M,d,k):
+    
+    #Create ULA array manifold
+    ff = [] 
+    A = np.zeros((len(k),181,M),dtype='complex')
+    ki = 0
     for kk in k:
-       ff = round(cc * kk / (2*np.pi),1)
-       A = np.zeros((M,181),dtype='complex')
+       ff.append(round(cc * kk / (2*np.pi),1))
        for mm in range(M):
             for azm in range(181):
-                A[mm,azm] = np.exp(-1j * mm * kk * d * np.sin(azm))  
+                A[ki,azm,mm] = np.exp(-1j * mm * kk * d * np.sin(azm))  
+       ki = ki + 1
+    
+    elev = [90.0]
+    azm = range(181)
+    
+    #*********************
+    #   Create HDF5 file
+    #*********************
+    f5 = h5py.File("../build/ULA.hdf5","w")
+    f5.create_group("Frequency (MHz)")
+    f5.create_dataset("listFreq", data = np.asarray(ff))
+    f5.create_dataset("listElev", data = np.asarray(elev))
+    f5.create_dataset("listAzm", data = np.asarray(azm))
+    
+    for fii in ff:
+        gFreq = f5.create_group(str(fii))
+        gFreq.create_group("Elevation (degrees)")
+        for th in elev:
+            gElev = gFreq.create_group(str(th))
+
+    for fi in range(len(ff)):
+            gElev = f5[str(ff[fi])][str(elev[0])]
+            magSet = gElev.create_dataset("Magnitude", data = (np.abs(A[fi,:,:])))
+            phaseSet = gElev.create_dataset("Phase", data = (np.angle(A[fi,:,:])))
+            hermSet = gElev.create_dataset("Hermitian", data = -1 *(np.angle(A[fi,:,:])))
+
+            #Label Dimensions
+            magSet.dims[0].label = "Azimuth"
+            magSet.dims[1].label = "Element"
+            phaseSet.dims[0].label = "Azimuth"
+            phaseSet.dims[1].label = "Element"
+            hermSet.dims[0].label = "Azimuth"
+            hermSet.dims[1].label = "Element"
+
     print("Hello from Uniform Linear Array")
 
 #omni-directional uniform circular array
--- Python/main.py
+++ Python/main.py
@@ -12,31 +12,33 @@ SOI = [30]
 #Signal to Noise Ratio (dB)
 SNR = 30
 
-fp = '../build/testnec.h5'
-dp = '/289.8/90.0/'
-f5 = h5py.File("../build/testnec.h5","r")
+#fp = '../build/testnec.h5'
+fp = '../build/ULA.hdf5'
+#dp = '/289.8/90.0/'
+dp = '/299.8/90.0/'
+f5 = h5py.File(fp,"r")
 
 #get Azimuths
-azm = np.asarray(f5['/listAzm'])
+azm = np.asarray(f5['/listAzm']).T
 
 #get Array Manifold
 d5 = f5[dp]
-AM_mag = (np.asarray(d5['Magnitude']))
-AM_phase = (np.asarray(d5['Phase']))
-AM_herm = (np.asarray(d5['Hermitian']))
+AM_mag = (np.asarray(d5['Magnitude'])).T
+AM_phase = (np.asarray(d5['Phase'])).T
+AM_herm = (np.asarray(d5['Hermitian'])).T
 
 #create signal
 x = AM_mag[:,SOI] * np.exp(1j * AM_phase[:,SOI])
 noise = 10**(-SNR / 10)
 #x += (0.001 * np.min(abs(x)) * noise * np.random.randn(len(x),1))
-Rxx = np.dot(x,x.conj().T)
+Rxx = np.dot(x,x.conj().T) + noise * np.eye(5)
 
 Pds = ds.getSpectrum(Rxx,AM_mag,AM_herm)
 Pmvdr = mvdr.getSpectrum(Rxx,AM_mag,AM_herm)
 Pmusic = MUSIC.getSpectrum(Rxx,AM_mag,AM_herm,len(SOI))
 
-mvdr.hello()
-plt.plot(azm,Pmvdr)
+MUSIC.hello()
+plt.plot(azm,Pmusic)
 plt.show()
 
 print("Hello World")
