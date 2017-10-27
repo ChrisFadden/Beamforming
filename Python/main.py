5fc7ec911aacaa2301f99e9639950fd7f0e1a5fe
George Burdell <gburdell@localhost.localdomain>
2017-10-27 13:40:24 -0400

    Made plot more accurate

--- Python/main.py
+++ Python/main.py
@@ -15,23 +15,26 @@ def plot_DF_Spectrum(P, azm):
 
 def plot_Beamform_Spectrum(w,AM_mag,AM_phase,azm,idx):
     y = 0*azm 
+    AM_mag[:,idx:idx + len(azm)] = AM_mag[:,idx:idx + len(azm)] / np.max(AM_mag[:,idx:idx+len(azm)])
     for ii in range(len(azm)):
         y[ii] = np.abs(np.dot(w.conj(), AM_mag[:,idx + ii] * np.exp(1j * AM_phase[:,idx + ii])))
     y = y / np.max(y)
-    plt.plot(azm,20*np.log10(y))
+    
+    offset = int(np.round(0.5*len(azm))) 
+    plt.plot(azm - offset,20*np.log10(np.roll(y,offset)))
     plt.show()
 
 #***********************
 #   Simulation Parameters
 #***********************
 #Signal of Interest
-SOI = [90]
+SOI = [0]
 
 #Signal to Noise Ratio (dB)
 SNR = 90
 
-#fp = '../build/Test.h5'
-fp = '../build/ULA.h5'
+fp = '../build/Test.h5'
+#fp = '../build/ULA.h5'
 
 f5 = h5py.File(fp,"r")
 
@@ -51,7 +54,10 @@ AM_phase = (np.asarray(f5['/Phase'])).T
 #   Create Signal
 #**********************
 
-soiIdx = 1*len(freq) * 0*len(elev) + SOI[0]
+freqIdx = 1;
+elevIdx = 0;
+
+soiIdx = freqIdx*elevIdx*len(azm) +  SOI[0]
 
 x = AM_mag[:,soiIdx] * np.exp(1j * AM_phase[:,soiIdx])
 
@@ -69,10 +75,11 @@ Pmusic = MUSIC.getSpectrum(Rxx,AM_mag,-1*AM_phase,len(SOI))
 #*****************
 #   Beamforming
 #*****************
+
 wmvdr = mvdr.getWeights(np.eye(5),AM_mag[:,soiIdx],AM_phase[:,soiIdx])
 wAF = AF.getWeights(5,0.5,2*np.pi,SOI[0])
-
-plot_Beamform_Spectrum(wmvdr,AM_mag,AM_phase,azm,0)
+wones = np.ones((1,5))
+plot_Beamform_Spectrum(wones,AM_mag,AM_phase,azm,soiIdx - SOI[0])
 
 #Plot Spectrum
 #plot_DF_Spectrum(Pmusic,azm)
