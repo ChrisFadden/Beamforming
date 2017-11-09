143b28a84f9575a5645a631edd084c58f2430656
George Burdell <gburdell@localhost.localdomain>
2017-11-09 16:11:11 -0500

    2D heatmap I think is ready for publication

--- Analysis/plot2D.py
+++ Analysis/plot2D.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#******************
+#   Read in data
+#******************
 fp = '../build/weights_2D.h5'
 
 f5 = h5py.File(fp,"r")
@@ -10,5 +13,27 @@ azm = np.asarray(f5['/listAzm'])
 elev = np.asarray(f5['/listElev'])
 mag = np.asarray(f5['/Magnitude'])
 
-sns.heatmap(mag,cmap = "viridis",square=True,xticklabels=30,yticklabels=30,vmin=-30,vmax=0)
+#*******************
+#   Set asthetics
+#*******************
+plt.rcParams["font.weight"] = "bold"
+plt.rcParams["axes.labelweight"] = "bold"
+
+#*******************
+#   Create heatmap
+#*******************
+sns.heatmap(mag,cmap = "viridis",
+    cbar_kws={"shrink": 0.40,"aspect": 10,"label": "Magnitude (dB)"},
+    square=True,xticklabels=30,yticklabels=30,vmin=-30,vmax=0,
+    rasterized=True)
+
+#******************
+#   Add titles and
+#   labels
+#******************
+plt.title("2D Beamformed Response",fontsize=14)
+plt.xlabel("Range (m)")
+plt.ylabel("Cross-Range (m)")
+
 plt.show()
+#plt.savefig("Heatmap.pdf",bbox_inches='tight')
